140c96a2ac7c3b20599a4c03ce2584deffddd894
Gleb <zadachings@gmail.com>
2018-04-12 18:13:24 +0300

    Add project files.

---
+++ CaesarCipher/Crypt.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CaesarCipher
+{
+    class Crypt
+    {
+        private SymmetricAlgorithm curCrypto;
+        private AesCryptoServiceProvider aesCrypto;
+        private TripleDESCryptoServiceProvider tripleCrypto;
+        private DESCryptoServiceProvider desCrypto;
+        public Crypt()
+        {
+            aesCrypto = new AesCryptoServiceProvider();
+            tripleCrypto = new TripleDESCryptoServiceProvider();
+            desCrypto = new DESCryptoServiceProvider();
+            aesCrypto.GenerateKey();
+            aesCrypto.GenerateIV();
+            tripleCrypto.GenerateKey();
+            tripleCrypto.GenerateIV();
+            desCrypto.GenerateIV();
+            desCrypto.GenerateKey();
+        }
+
+
+        private void Selection(ref SymmetricAlgorithm crypto, string mode)
+        {
+            switch (mode)
+            {
+                case "CBC":
+                    crypto.Mode = CipherMode.CBC;
+                    break;
+                case "CFB":
+                    crypto.Mode = CipherMode.CFB;
+                    break;
+                case "ECB":
+                    crypto.Mode = CipherMode.ECB;
+                    break;
+                default:
+                    crypto.Mode = CipherMode.CBC;
+                    break;
+            }
+        }
+
+        public void SelectionCipher(ref SymmetricAlgorithm crypto, string algoritm)
+        {
+            switch (algoritm)
+            {
+                case "DES":
+                    crypto = desCrypto;
+                    break;
+                case "TripleDES":
+                    crypto = desCrypto;
+                    break;
+                case "AES":
+                    crypto = aesCrypto;
+                    break;
+                default:
+                    crypto = desCrypto;
+                    break;
+            }
+        }
+        
+        public string Encrypt(string key,string IV, string opentext, string mode, string cipher)
+        {
+
+            //crypto.GenerateKey();//.Key = ASCIIEncoding.ASCII.GetBytes(key);
+            //crypto.GenerateIV();//.IV = ASCIIEncoding.ASCII.GetBytes(IV);
+            //if (mode.Equals("CTS") || mode != "OFB")
+            //    Selection(ref crypto, mode);
+            //triplecrypto.Mode = CipherMode.CTS;
+
+
+            SelectionCipher(ref curCrypto, cipher);
+            Selection(ref curCrypto, mode);
+            
+            FileStream fs = new FileStream(@"d:\crypt.txt", FileMode.OpenOrCreate, FileAccess.Write);
+            
+            CryptoStream cryptoStream = new CryptoStream(fs, curCrypto.CreateEncryptor(), CryptoStreamMode.Write);
+            byte[] data = ASCIIEncoding.ASCII.GetBytes(opentext);
+            cryptoStream.Write(data, 0, data.Length);
+            fs.Flush();
+            cryptoStream.Flush();
+            cryptoStream.Close();
+            fs.Close();
+            
+            return fs.Name;
+        }
+
+        public string Decrypt(string key, string IV, string ciphertext, string mode)
+        {
+            //CryptGenRandom
+            //DESCryptoServiceProvider crypto = new DESCryptoServiceProvider();
+            //crypto.Key = ASCIIEncoding.ASCII.GetBytes(key);
+            //crypto.GenerateIV();//.IV = ASCIIEncoding.ASCII.GetBytes(IV);
+            //crypto.GenerateKey();
+            //Selection(ref crypto, mode);
+            FileStream fs = new FileStream(@"d:\crypt.txt", FileMode.Open, FileAccess.Read);
+            CryptoStream cryptoStream = new CryptoStream(fs,curCrypto.CreateDecryptor(),CryptoStreamMode.Read);
+            StreamReader reader = new StreamReader(cryptoStream,Encoding.Default);
+            string data = reader.ReadToEnd();
+            fs.Flush();
+            cryptoStream.Flush();
+            fs.Close();
+            cryptoStream.Close();
+            return data;
+        }
+    }
+}
---
+++ CaesarCipher/MainWindow.xaml.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace CaesarCipher
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public string Selection()
+        {
+            if (CBC.IsSelected)
+                return "CBC";
+            if (CFB.IsSelected)
+                return "CFB";
+            if (ECB.IsSelected)
+                return "ECB";
+            else return "CBC";
+        }
+
+        public string SelectionCipher()
+        {
+            if (DES.IsSelected)
+                return "DES";
+            if (TripleDES.IsSelected)
+                return "TripleDES";
+            if (AES.IsSelected)
+                return "AES";
+            else return "DES";
+        }
+
+        private Crypt crypt = new Crypt();
+        private void OpenFile_Click(object sender, RoutedEventArgs e)
+        {
+            FileDialog dialog = new OpenFileDialog();
+            dialog.Filter = "Text Files | *.txt";
+            dialog.FilterIndex = 1;
+            if (dialog.ShowDialog() == true)
+                filearea.Text = File.ReadAllText(dialog.FileName);
+
+        }
+
+        private void SaveFile_Click(object sender, RoutedEventArgs e)
+        {
+            FileDialog dialog = new SaveFileDialog();
+            dialog.Filter = "Text Files | *.txt";
+            if (dialog.ShowDialog() == true)
+                File.WriteAllText(dialog.FileName, filearea.Text);
+        }
+
+        private void About_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Made by Zadachin Gleb, TM-51, #9");
+        }
+
+        private void Encrypt_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                filearea.Text = File.ReadAllText(crypt.Encrypt(Key.Text, IV.Text, filearea.Text,Selection(), SelectionCipher()),Encoding.Default);
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                //MessageBox.Show("Key and IV must contain only 8 symbols");
+            }
+        }
+
+        private void Decrypt_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                filearea.Text = crypt.Decrypt(Key.Text, IV.Text, filearea.Text,Selection());
+                File.Delete(@"d:\crypt.txt");
+            }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                //MessageBox.Show("Key and IV must contain only 8 symbols");
+            }
+        }
+    }
+}
